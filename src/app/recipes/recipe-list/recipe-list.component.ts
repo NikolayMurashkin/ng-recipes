fbbe53af4b5617d5773ab4565897f017e24f3183
Nikolay <m.nikolay1990@gmail.com>
2024-05-14 17:51:53 +0700

    add recipe-details layout, recipe-list layout

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.scss'
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://media.cooky.vn/images/blog-2016/tim-hieu-cac-loai-bot-lam-banh-giup-cac-chi-em-chon-dung-bot-lam-dung-banh%2070.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://assets.bonappetit.com/photos/57acc9d3f1c801a1038bc775/master/w_4000,h_2667,c_limit/real-tuna-casserole-USE-THIS.jpg'),
+  ];
 }
